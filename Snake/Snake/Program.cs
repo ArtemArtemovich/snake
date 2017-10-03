eaf18cc479cfd193bf74f354c023ee7343f6dd1a
Shaidenko <splgroup@ya.ru>
2017-10-03 19:24:31 +0300

    Созданы вертикальная и горизонтальная линии

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,12 +10,14 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
 
-            Point p2 = new Point(3, 6, '#');
-            p2.Draw();
 
+            HorizotalLine line = new HorizotalLine( 1, 10, 8, '+' );
+            line.Drow();
+
+            VerticalLine line1 = new VerticalLine( 1, 40, 1, '+' );
+            line1.Drow();
+            
             Console.ReadLine();
         }
     }
---
+++ Snake/Snake/VerticalLine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class VerticalLine
+    {
+        List<Point> pList;
+
+        public VerticalLine(int yUpper, int yLower, int x, char sym)
+        {
+            pList = new List<Point>();
+            for (int y = yUpper; y <= yLower; y++)
+            {
+                Point p = new Point( x, y, sym );
+                pList.Add(p);
+            }
+        }
+
+        public void Drow()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
